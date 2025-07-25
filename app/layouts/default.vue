4d606d6b843831dc676a9261a46224d9ec037398
Wilson Su <wilsonsu2008@hotmail.com>
2025-07-26 00:56:03 +0800

    chore: better window titlebar styling

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { getCurrentWindow } from '@tauri-apps/api/window';
+
+const appWindow = getCurrentWindow();
+</script>
+
+<template>
+<!--    <div class="absolute top-0 right-0 left-0 flex items-center p-2 w-full h-fit" data-tauri-drag-region>-->
+<!--        <UButton icon="i-lucide-x" color="neutral" variant="ghost" size="sm" @click="() => {appWindow.close()}"/>-->
+<!--        <div class="flex-grow"/>-->
+<!--    </div>-->
+    <slot/>
+</template>
+
+<style scoped>
+
+</style>
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,20 @@
+use tauri::Manager;
+
+use tauri_plugin_decorum::WebviewWindowExt; // adds helper methods to WebviewWindow
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_decorum::init()) // initialize the decorum plugin
     .setup(|app| {
+        // Create a custom titlebar for main window
+        // On Windows this will hide decoration and render custom window controls
+        // On macOS it expects a hiddenTitle: true and titleBarStyle: overlay
+        let main_window = app.get_webview_window("main").unwrap();
+        main_window.create_overlay_titlebar().unwrap();
+
+        #[cfg(target_os = "macos")]
+        main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
       if cfg!(debug_assertions) {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
